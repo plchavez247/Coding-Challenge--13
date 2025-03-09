e39997e0b7f294e3e4b5e85c0ee14d7c55af0bd1
Pablo Chavez <plchavez@usf.edu>
2025-03-09 11:04:57 -0400

    Task 1-Fixed

--- cc_13.js
+++ cc_13.js
@@ -1,3 +1,20 @@
 function newEmployeeCard(name, position) {
+    const employeeContainer = document.getElementById("employeeContainer")
+    //getElementById employeeContainer
+    let card = document.createElement("div");
+    card.setAttribute("class", "employee-card");
+    card.setAttribute("id", `employee-${employeeCount}`);
+    card.innerHTML = "<h2>${name}</h3><p>${position}</p>";
     
+    //creatElement div tag
+    // classList.add employee-card
+    
+    // innerHTML
+    // appendChild to container
+    
+    // use examples
+    //addEmployeeCard("Alice", "Software Engineer")
+    //
+
+    })
 }
