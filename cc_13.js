767f8dd68432af8e9ca8d623f5c80eb3f96186ac
Pablo Chavez <plchavez@usf.edu>
2025-03-09 19:38:23 -0400

    Task 4- Employee Card Removal with Event Bubbling

--- cc_13.js
+++ cc_13.js
@@ -13,7 +13,7 @@ function newEmployeeCard(name, position) {
     removeButton.textContent = `Remove Employee`;
     removeButton.addEventListener(`click`, function(event){
         event.stopPropagation();
-        card.remove();
+        employeeContainer.removeChild(card);
     }); //creating remove button for deleting card
     card.appendChild(removeButton);
     employeeCount +=1;// adding to employee count when adding a new card
@@ -24,6 +24,18 @@ function newEmployeeCard(name, position) {
 
     //Task 3:Converting NodeLists to Arrays for Bulk Updates
 
-    const employeeCards = document.querySelectorAll(".employeeCard");//using document.querySelectorAll
-    
-
+    const employeeCards = document.querySelectorAll(".employee-card");//using document.querySelectorAll
+    const employeeCardsArray = Array.from(employeeCards);// converting into an array with Array.from
+    employeeCardsArray.forEach(card => {
+        card.style.backgroundColor = "lightgreen";
+        card.style.border = "2px solid darkgreen"
+        
+    });
+//Task 4: Implementing Remobal of Employee Cards with Event Bubbling
+const employeeContainer = document.getElementById("employeeContainer");
+employeeContainer.addEventListener("click", (event)=> {
+    const card = event.target.closest(`.employeeCard`);//event listener for whrn card is clicked
+    if(card) {
+        alert(`Employee card clicked: ${card.id}`);//message when card is clicked
+    }
+});
