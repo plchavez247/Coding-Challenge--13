6b96f20a4d54314c103857bc17aa2ae9f42243da
Pablo Chavez <plchavez@usf.edu>
2025-03-09 11:13:58 -0400

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,10 +1,20 @@
+let employeeCount = 0 //Cards start at 0 
 function newEmployeeCard(name, position) {
     const employeeContainer = document.getElementById("employeeContainer")
     //getElementById employeeContainer
     let card = document.createElement("div");
     card.setAttribute("class", "employee-card");
-    card.setAttribute("id", `employee-${employeeCount}`);
-    card.innerHTML = "<h2>${name}</h3><p>${position}</p>";
+    card.setAttribute("id", `employee-${employeeCount}`);//setting attributes
+    card.innerHTML = `<h2>${name}</h3><p>${position}</p>`;//create heading for name and paragrah for position
+    employeeContainer.appendChild(card);//append the employee card
+
+    const removeButton = document.createElement(`button`);
+    removeButton.textContent = `Remove Employee`;
+    removeButton.addEventListener(`click`, function(event){
+        event.stopPropagation();
+        card.remove();
+    }); //creating remove button for deleting card
+    card.appendChild(removeButton);
     
     //creatElement div tag
     // classList.add employee-card
