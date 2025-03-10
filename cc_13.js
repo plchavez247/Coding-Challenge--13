69836fafc7e6b94f96ffbbcea987864d2e87be77
Pablo Chavez <plchavez@usf.edu>
2025-03-09 20:46:46 -0400

    Task 5 - Inline Editing for Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -9,6 +9,14 @@ function newEmployeeCard(name, position) {
     card.innerHTML = `<h2>${name}</h2><p>${position}</p>`;//create heading for name and paragrah for position
     employeeContainer.appendChild(card);//append the employee card
 
+    const editButton = document.createElement(`button`);
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", function(event){
+        event.stopPropagation();
+        cardEditing(card);
+    });
+    card.appendChild(editButton);
+
     const removeButton = document.createElement(`button`);
     removeButton.textContent = `Remove Employee`;
     removeButton.addEventListener(`click`, function(event){
@@ -31,11 +39,39 @@ function newEmployeeCard(name, position) {
         card.style.border = "2px solid darkgreen"
         
     });
-//Task 4: Implementing Remobal of Employee Cards with Event Bubbling
+//Task 4: Implementing Removal of Employee Cards with Event Bubbling
 const employeeContainer = document.getElementById("employeeContainer");
 employeeContainer.addEventListener("click", (event)=> {
     const card = event.target.closest(`.employee-card`);//event listener for whrn card is clicked
     if(card) {
-        alert(`Employee card clicked: ${card.id}`);//message when card is clicked
+        console.log(`Employee card clicked: ${card.id}`);//message when card is clicked
     }
 });
+
+//Task 5: Inline Editing of Employee Details
+function cardEditing(card) {
+    const nameElement = card.querySelector(`h2`);
+    const positionElement = card.querySelector(`p`);
+
+    const nameInput = document.createElement(`input`);
+    nameInput.value =nameElement.textContent;// creating input fields for name
+
+    const positionInput = document.createElement(`input`);
+    positionInput.value = positionElement.textContent;// creating input fields for position
+
+    nameElement.textContent = ``;
+    nameElement.appendChild(nameInput);//replacing the text with inputs for name
+
+    positionElement.textContent= ``;
+    positionElement.appendChild(positionInput);//replacing text with input for position
+
+    const saveButton = document.createElement(`button`);
+    saveButton.textContent = `Save`//making a save button
+
+    card.appendChild(saveButton);
+    saveButton.addEventListener("click", function(){
+        nameElement.textContent = nameInput.value;
+        positionElement.textContent = positionInput.value;
+        card.removeChild(saveButton);
+    });
+};
